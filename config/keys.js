4c376e847c1cbb08ba1a32a1cdd9d4aa23119b39
ravibalg-aj <aravibalaji@gmail.com>
2020-06-01 19:36:44 +0530

    .gitignore changes

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://Ravibalaji:purveyit@civil-registry-shard-00-00-uta2q.mongodb.net:27017,civil-registry-shard-00-01-uta2q.mongodb.net:27017,civil-registry-shard-00-02-uta2q.mongodb.net:27017/purveyit?ssl=true&replicaSet=Civil-Registry-shard-0&authSource=admin&retryWrites=true&w=majority",
-  secretOrKey: "secret",
-};
-
-// mongodb://Ravibalaji:<password>@civil-registry-shard-00-00-uta2q.mongodb.net:27017,civil-registry-shard-00-01-uta2q.mongodb.net:27017,civil-registry-shard-00-02-uta2q.mongodb.net:27017/test?ssl=true&replicaSet=Civil-Registry-shard-0&authSource=admin&retryWrites=true&w=majority
-// mongodb+srv://Ravibalaji:purveyit@civil-registry-uta2q.mongodb.net/purveyit?retryWrites=true&w=majority
